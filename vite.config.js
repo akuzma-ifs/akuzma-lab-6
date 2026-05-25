71bbf4679ab4e69063fde2c5a8a172e2234699ba
akuzma-ifs <ameliakuzma@wp.pl>
2026-05-26 00:15:42 +0200

    konfiguracja github pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/lab5-ak-ifs/',
-
+  base: '/akuzma-lab-6/',
   plugins: [
     tailwindcss(),
   ],
